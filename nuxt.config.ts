264fb8a1cae2f7e8d932d5656a92c1db34b65c0e
dudd1ts <kserg90@gmail.com>
2023-05-07 18:20:34 +0300

    Initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,7 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  imports: {
+    autoImport: false,
+  },
+  modules: ["@nuxtjs/tailwindcss", "@vueuse/nuxt"],
+});
